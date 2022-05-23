796a4535317bdcf67394eeea33a0c27a262316ff
Wing117 <axboyd72@gmail.com>
2022-05-23 18:52:51 -0400

    .json {} addition, .js init for json parsing

---
+++ creaturegenerator.js
@@ -0,0 +1,69 @@
+// NOTE: This does not work currently. Do not try to run it.
+
+<script>
+
+// Immediately-invoked function expression
+(function() {
+  // Load the script
+  const script = document.createElement("script");
+  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+  script.type = 'text/javascript';
+  script.addEventListener('load', () => {
+    console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
+    // use jQuery below
+  });
+  document.head.appendChild(script);
+})();
+
+const Typer = {
+    async type(selector, text, speed = 1) {
+        document.querySelector(selector).innerText = "";
+        const chunks = text.split(".");
+        let characterCount = 0;
+        for (const chunk of chunks) {
+            const chunkText = chunk + ".";
+            const characters = chunkText.split("");
+            for (const character of characters) {
+                characterCount++;
+                document.querySelector(selector).innerText = text.substr(
+                0,
+                characterCount
+                );
+                await wait(speed);
+            }
+        await wait(175);
+        }
+    },
+};
+
+async function loadCreatures() {
+  const response = await fetch('https://raw.githubusercontent.com/PandionGames/bandasgrove/main/bg_creatures.json');
+  const creaturelist = await response.json();
+  console.log(creaturelist); 
+};
+
+const creaturelist = loadCreatures();
+
+
+// var creatures = JSON.parse(creaturelist, function (key, value) {
+     return value;
+  });
+
+const QuestionOracle = {
+    isTyping: false,
+    async ask() {
+        if (this.isTyping) return;
+        this.isTyping = true;
+        // const rng = Math.floor(Math.random() * creatureslist.length-1);
+        // const index = rng.padstart(3,"0");
+        let displaytext =  creatures.description;
+        await Typer.type("#creaturegeneratortext", displaytext, 25);
+        this.isTyping = false;
+    },
+};
+
+
+async function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+</script>

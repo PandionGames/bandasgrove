98efde724bf2cb597348d380aa19517b414bc3b8
Wing117 <axboyd72@gmail.com>
2022-05-23 21:19:13 -0400

    IT WORKS

--- creaturegenerator.js
+++ creaturegenerator.js
@@ -1,55 +1,56 @@
-// NOTE: This does not work currently. Do not try to run it.
-
 <script>
-const Typer = {
-    async type(selector, text, speed = 1) {
-        document.querySelector(selector).innerText = "";
-        const chunks = text.split(".");
-        let characterCount = 0;
-        for (const chunk of chunks) {
-            const chunkText = chunk + ".";
-            const characters = chunkText.split("");
-            for (const character of characters) {
-                characterCount++;
-                document.querySelector(selector).innerText = text.substr(
-                0,
-                characterCount
-                );
-                await wait(speed);
+    const Typer = {
+        async type(selector, text, speed = 1) {
+            document.querySelector(selector).innerText = "";
+            const chunks = text.split(".");
+            let characterCount = 0;
+            for (const chunk of chunks) {
+                const chunkText = chunk + ".";
+                const characters = chunkText.split("");
+                for (const character of characters) {
+                    characterCount++;
+                    document.querySelector(selector).innerText = text.substr(
+                    0,
+                    characterCount
+                    );
+                    await wait(speed);
+                }
+            await wait(175);
             }
-        await wait(175);
-        }
-    },
-};
+        },
+    };
 
-async function loadCreatures() {
-  const response = await fetch('https://raw.githubusercontent.com/PandionGames/bandasgrove/main/bg_creatures.json');
-  const creaturelist = await response.json();
-  console.log(response); 
-};
+    async function spotcreature() {
+    const response = await fetch('https://raw.githubusercontent.com/PandionGames/bandasgrove/main/bg_creatures.json');
+    const creaturelist = await response.json();
 
-const creaturelist = loadCreatures();
+    const rng = Math.floor(Math.random() * creaturelist.creatures.length-1);
+    // console.log(creaturelist.creatures);
+    console.log(creaturelist.creatures[rng].index);
+    console.log(creaturelist.creatures[rng].name);
+    console.log(creaturelist.creatures[rng].description);
+    const spottedcreature = creaturelist.creatures[rng];
+    return spottedcreature;
+    };
+
+    const QuestionOracle = {
+        isTyping: false,
+        async ask() {
+            if (this.isTyping) return;
+            this.isTyping = true;
+            const spottedcreature = await spotcreature();
+            console.log(spottedcreature);
+            console.log(spottedcreature["name"]);
+            console.log(spottedcreature["description"]);
+            console.log(spottedcreature["index"]);
+            let displaytext =  spottedcreature["index"] + " | " + spottedcreature["name"] + " | " + spottedcreature["description"];
+            await Typer.type("#creaturegeneratortext", displaytext, 25);
+            this.isTyping = false;
+        },
+    };
 
 
-var creatures = JSON.parse(creaturelist, function (key, value) {
-     return value;
-  });
-
-const QuestionOracle = {
-    isTyping: false,
-    async ask() {
-        if (this.isTyping) return;
-        this.isTyping = true;
-        // const rng = Math.floor(Math.random() * creatureslist.length-1);
-        // const index = rng.padstart(3,"0");
-        let displaytext =  creatures.description;
-        await Typer.type("#creaturegeneratortext", displaytext, 25);
-        this.isTyping = false;
-    },
-};
-
-
-async function wait(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-</script>
+    async function wait(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    </script>

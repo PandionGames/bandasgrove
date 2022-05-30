1a81c0ac4148f29b3e119918035684b8390d625a
Wing117 <axboyd72@gmail.com>
2022-05-30 16:31:36 -0400

    Working state

--- ConvergenceEvent.js
+++ ConvergenceEvent.js
@@ -2,13 +2,8 @@
     async function pickaplane() {
         const response = await fetch('https://raw.githubusercontent.com/PandionGames/bandasgrove/main/bg_convergenceevents.json');
         const planelist = await response.json();
-        console.log(planelist);
-        console.log(planelist.planes);
-        console.log(planelist.planes.length);
-        const rng = Math.floor(Math.random() * planelist.planes.length-1);
-        console.log(planelist.planes[0][0]);
-        const featurepick = Math.floor(Math.random() * planelist.planes[rng].length-1);
-        console.log(planelist.planes[rng][featurepick])
+        const rng = Math.floor(Math.random() * planelist.planes.length);
+        const featurepick = Math.floor(Math.random() * Object.keys(planelist.planes[rng].features[0]).length-1);
         return planelist.planes[rng];
     };
 
@@ -19,22 +14,18 @@
             this.isTyping = true;
             
             // clean up the fields
-            // document.querySelector("#indexoutput").innerHTML = '<span style="font-weight:bold;">Index: </span>';
-            // document.querySelector("#nameoutput").innerHTML = '<span style="font-weight:bold;">Name: </span>';
-            // document.querySelector("#planeoutput").innerHTML = '<span style="font-weight:bold;">Plane: </span>';
-            // document.querySelector("#descriptionoutput").innerHTML  = '<span style="font-weight:bold;">Description: </span>';
+            document.querySelector("#planefragmentoutput").innerHTML = '<span style="font-weight:bold;">Plane: </span>';
+            document.querySelector("#fragmentfeatureoutput").innerHTML = '<span style="font-weight:bold;">Description: </span>';
 
-            // call for a random creature, store the array.
+            // call for a random plane and calculate number of features available
             const plane = await pickaplane();
+            const featurepick = Math.floor(Math.random() * Object.keys(plane.features[0]).length-1);
 
             // call the individual key:values of spottedcreature and send them to the typer, 
             // which writes the value in the text fields.
-            await Typer.type("#planefragmentoutput",plane, 15);
+            await Typer.type("#planefragmentoutput",plane.name, 15);
+            await Typer.type("#fragmentfeatureoutput",plane.features[0][featurepick], 15);
             this.isTyping = false;
         },
     };
-
-    async function wait(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
 </script>

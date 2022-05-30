809f139d1176244288076e6f6a09ef7673eca6c5
Wing117 <axboyd72@gmail.com>
2022-05-30 17:48:29 -0400

    production code

--- ConvergenceEvent.js
+++ ConvergenceEvent.js
@@ -3,7 +3,6 @@
         const response = await fetch('https://raw.githubusercontent.com/PandionGames/bandasgrove/main/bg_convergenceevents.json');
         const planelist = await response.json();
         const rng = Math.floor(Math.random() * planelist.planes.length);
-        const featurepick = Math.floor(Math.random() * Object.keys(planelist.planes[rng].features[0]).length-1);
         return planelist.planes[rng];
     };
 
@@ -14,17 +13,18 @@
             this.isTyping = true;
             
             // clean up the fields
-            document.querySelector("#planefragmentoutput").innerHTML = '<span style="font-weight:bold;">Plane: </span>';
-            document.querySelector("#fragmentfeatureoutput").innerHTML = '<span style="font-weight:bold;">Description: </span>';
+            document.querySelector("#planefragmentoutput").innerHTML = '<span style="font-weight:bold;">Plane Detected: </span>';
+            document.querySelector("#fragmentfeatureoutput").innerHTML = '<span style="font-weight:bold;">Unique Feature: </span>';
 
             // call for a random plane and calculate number of features available
             const plane = await pickaplane();
-            const featurepick = Math.floor(Math.random() * Object.keys(plane.features[0]).length-1);
+            const featurepick = Math.floor(Math.random() * plane.features.length);
+            console.log(featurepick);
 
             // call the individual key:values of spottedcreature and send them to the typer, 
             // which writes the value in the text fields.
             await Typer.type("#planefragmentoutput",plane.name, 15);
-            await Typer.type("#fragmentfeatureoutput",plane.features[0][featurepick], 15);
+            await Typer.type("#fragmentfeatureoutput",plane.features[featurepick], 15);
             this.isTyping = false;
         },
     };

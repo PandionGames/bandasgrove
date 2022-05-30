98f1ef0d1a92c806229b456a1469b5dfc0dc914b
Wing117 <axboyd72@gmail.com>
2022-05-30 15:31:03 -0400

    Testing

--- ConvergenceEvent.js
+++ ConvergenceEvent.js
@@ -1,21 +1,4 @@
 <script>
-const Typer = {
-        async type(selector, text, speed = 1) {
-            const chunks = text.split(".");
-            let characterCount = 0;
-            for (const chunk of chunks) {
-                const chunkText = chunk + ".";
-                const characters = chunkText.split("");
-                for (const character of characters) {
-                    document.querySelector(selector).innerHTML += text.substr(characterCount,1);
-                    characterCount++;
-                    await wait(speed);
-                }
-            await wait(175);
-            }
-        },
-    };
-
     async function pickaplane() {
         const response = await fetch('https://raw.githubusercontent.com/PandionGames/bandasgrove/main/bg_convergenceevents.json');
         const eventlist = await response.json();
@@ -23,6 +6,7 @@ const Typer = {
         const planepick = Math.floor(Math.random() * eventlist.planes.length-1);
         console.log(eventlist.planes[planepick]);
         const featurepick = Math.floor(Math.random() * eventlist.planes[planepick].length-1);
+        console.log(eventlist.planes[planepick][featurepick])
         return eventlist.planes[rng];
     };
 
@@ -33,17 +17,17 @@ const Typer = {
             this.isTyping = true;
             
             // clean up the fields
-            document.querySelector("#indexoutput").innerHTML = '<span style="font-weight:bold;">Index: </span>';
-            document.querySelector("#nameoutput").innerHTML = '<span style="font-weight:bold;">Name: </span>';
-            document.querySelector("#planeoutput").innerHTML = '<span style="font-weight:bold;">Plane: </span>';
-            document.querySelector("#descriptionoutput").innerHTML  = '<span style="font-weight:bold;">Description: </span>';
+            // document.querySelector("#indexoutput").innerHTML = '<span style="font-weight:bold;">Index: </span>';
+            // document.querySelector("#nameoutput").innerHTML = '<span style="font-weight:bold;">Name: </span>';
+            // document.querySelector("#planeoutput").innerHTML = '<span style="font-weight:bold;">Plane: </span>';
+            // document.querySelector("#descriptionoutput").innerHTML  = '<span style="font-weight:bold;">Description: </span>';
 
             // call for a random creature, store the array.
             const plane = await pickaplane();
 
             // call the individual key:values of spottedcreature and send them to the typer, 
             // which writes the value in the text fields.
-            await Typer.type("#planeoutput",plane["feature"], 15);
+            await Typer.type("#planefragmentoutput",plane["feature"], 15);
             this.isTyping = false;
         },
     };
